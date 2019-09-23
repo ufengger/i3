86e7ababc1801237c472c08cae8d37ba66c15dd4
ufengger <haifeng.wu@foxmail.com>
2019-09-23 17:24:03 +0800

    don't start fcitx

--- misc.sh
+++ misc.sh
@@ -3,7 +3,9 @@
 # working computer setup
 if [ $USER == 'wuhaifeng' ]
 then
+    setxkbmap -option ctrl:ralt_rctrl -option ctrl:nocaps
     xmodmap -e "keycode 9 = Escape asciitilde Escape"
     xset s off -dpms
-    # fcitx
+else
+    setxkbmap -option ctrl:ralt_rctrl -option ctrl:nocaps
 fi
