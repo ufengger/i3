f5a791adceebaebe91c3f8bd48b8c5bf5767498e
ufengger <haifeng.wu@foxmail.com>
2019-09-25 16:09:38 +0800

    configure the xmodmap overwritten problem by fcitx

--- misc.sh
+++ misc.sh
@@ -3,6 +3,10 @@
 # working computer setup
 if [ $USER == 'wuhaifeng' ]
 then
+    # Fcitx now control keyboard layout and when switch layout, xmodmap setting
+    # will be overwritten. see:
+    # https://fcitx-im.org/wiki/FAQ#xmodmap_settings_being_overwritten
+    fcitx
     setxkbmap -option ctrl:ralt_rctrl -option ctrl:nocaps
     xmodmap -e "keycode 9 = Escape asciitilde Escape"
     xset s off -dpms
