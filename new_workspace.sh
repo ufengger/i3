63211757c7df7f2abf32a73e3178c853f931467c
ufengger <haifeng.wu@foxmail.com>
2021-05-19 16:55:05 +0800

    debug new_workspace

--- new_workspace.sh
+++ new_workspace.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-i3-msg workspace $(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | xargs ~/.config/i3/find_next.out)
+i3-msg workspace "$(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | xargs ~/.config/i3/find_next.out)"
