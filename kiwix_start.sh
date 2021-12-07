b057479428755fc35de6dcaddbddd378428fce6b
ufengger <haifeng.wu@foxmail.com>
2021-12-07 10:29:09 +0800

    update kiwix scripts

--- kiwix_start.sh
+++ kiwix_start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-kiwix-serve --library ~/wikidata/library_zim.xml --address 127.0.0.1 --port 8000 --daemon
+kiwix-serve --library ~/wikidata/library_zim.xml --port 8080 --daemon
+kiwix-serve --library ~/wikidata/library_wiki_en_all.xml --port 8000 --daemon
+kiwix-serve --library ~/wikidata/library_wiki_zh_all.xml --port 8040 --daemon
+kiwix-serve --library ~/wikidata/library_gutenberg.xml --port 8081 --daemon
